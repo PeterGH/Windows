cb09637b843185096e776d308ad55a93aafa79dc
Peter <peteryzufl@gmail.com>
2022-08-23 22:34:25 -0700

    test

--- TokenTest/TokenTest.cpp
+++ TokenTest/TokenTest.cpp
@@ -2,9 +2,47 @@
 #include <iostream>
 #include <string>
 
+DWORD GetTokenInformation(HANDLE tokenHandle, TOKEN_INFORMATION_CLASS infoClass, std::unique_ptr<byte[]>& info)
+{
+    DWORD error = ERROR_SUCCESS;
+    DWORD infoLength = 0;
+    if (GetTokenInformation(tokenHandle, infoClass, NULL, 0, &infoLength))
+    {
+        std::wcout << L"GetTokenInformation succeeded unexpectedly, infoLength " << infoLength << std::endl;
+        return ERROR_BAD_ARGUMENTS;
+    }
+
+    info.reset(new byte[infoLength]);
+
+    if (!GetTokenInformation(tokenHandle, infoClass, info.get(), infoLength, &infoLength))
+    {
+        error = GetLastError();
+        std::wcout << L"GetTokenInformation(" << tokenHandle << L", " << infoClass << L") failed with error " << error << std::endl;
+    }
+
+    return error;
+}
+
+DWORD GetTokenUserInfo(HANDLE tokenHandle)
+{
+    DWORD error = ERROR_SUCCESS;
+    std::unique_ptr<byte[]> buffer;
+    PTOKEN_USER user;
+
+    error = GetTokenInformation(tokenHandle, TokenUser, buffer);
+
+    if (error == ERROR_SUCCESS)
+    {
+        user = (PTOKEN_USER)buffer.get();
+        std::wcout << L"TokenUser " << user->User.Sid << std::endl;
+    }
+
+    return error;
+}
+
 int wmain(int argc, wchar_t* argv[])
 {
-    DWORD error = S_OK;
+    DWORD error = ERROR_SUCCESS;
     HANDLE tokenHandle = INVALID_HANDLE_VALUE;
 
     HANDLE processHandle = GetCurrentProcess();
@@ -30,10 +68,11 @@ int wmain(int argc, wchar_t* argv[])
     {
         std::wcout << L"OpenProcessToken succeeded" << std::endl;
 
+        error = GetTokenUserInfo(tokenHandle);
+
         if (!CloseHandle(tokenHandle))
         {
-            error = GetLastError();
-            std::wcout << L"CloseHandle(TokenHandle:" << tokenHandle << L") failed with error " << error << std::endl;
+            std::wcout << L"CloseHandle(TokenHandle:" << tokenHandle << L") failed with error " << GetLastError() << std::endl;
         }
     }
 
@@ -41,8 +80,7 @@ int wmain(int argc, wchar_t* argv[])
     {
         if (!CloseHandle(processHandle))
         {
-            error = GetLastError();
-            std::wcout << L"CloseHandle(ProcessHandle:" << processHandle << L") failed with error " << error << std::endl;
+            std::wcout << L"CloseHandle(ProcessHandle:" << processHandle << L") failed with error " << GetLastError() << std::endl;
         }
     }
 
